95aaa4dab10a8f4d945251a60abb38ea1a68270d
Vance Will <23724267+vencewill@users.noreply.github.com>
2022-04-17 19:34:57 +0800

    watchos

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftRSA",
-    platforms: [.iOS(.v13), .macOS(.v10_15)],
+    platforms: [.iOS(.v13), .macOS(.v10_15), .watchOS(.v6)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
